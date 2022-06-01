4b4dfc6c927f03b2d1c8e19cbbd1474b32ab77e6
Andreea Sinka <MY_NAME@example.com>
2022-06-01 19:52:02 +0300

    mathematical operators exercise

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,35 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
-        String name = "John";
+        System.out.println("Hello");
+        String name = "Andreea";
         System.out.println(name);
+        int x = 6;
+        int y = 12;
+        int sum = x + y;
+        System.out.println(sum);
+        int a = 15;
+        float b = 6;
+        float div = a/b;
+        System.out.println(div);
+        int c = -5;
+        int d = 8;
+        int e = 6;
+        System.out.println("result for a. is " + (c + (d * e)));
+        int f = 55;
+        int g = 9;
+        int h = 9;
+        System.out.println("result for b. is " + ((f + g) % h));
+        int i = 20;
+        int j = -3;
+        int k = 5;
+        float l = 8;
+        System.out.println("result for c. is " + (i + (j * k)/ l));
+        int m = 5;
+        int n = 15;
+        int o = 3;
+        int p = 2;
+        int q = 8;
+        int r = 3;
+        System.out.println("result for d. " + (m + (n/o * p) - q % r));
     }
 }
